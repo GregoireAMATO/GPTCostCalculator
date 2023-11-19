113dce187915a9229f7c49744873284eef7da2b9
GregoireAMATO <61666789+GregoireAMATO@users.noreply.github.com>
2023-11-19 12:09:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GPTCostCalculator',
-    version='0.2.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'tiktoken',  # Ajoutez ici toutes les dépendances nécessaires
